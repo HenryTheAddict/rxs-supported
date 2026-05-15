ad86ef7aab721299f2383fd34c21ec75ad79554f
PrivacyIsARight <vexalous@gmail.com>
2026-05-14 20:47:12 -0400

    Remove empty entries from backend CMake lists

--- src/backend/backend.cmake
+++ src/backend/backend.cmake
@@ -5,11 +5,9 @@ include(src/backend/common/common.cmake)
 set(HEADERS_BACKEND
     "${HEADERS_BACKEND_COMMON}"
     "${HEADERS_BACKEND_CPU}"
-    ""
    )
 
 set(SOURCES_BACKEND
     "${SOURCES_BACKEND_COMMON}"
     "${SOURCES_BACKEND_CPU}"
-    ""
    )
