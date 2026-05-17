2eed04d44df9261509eec839eb2b28467264912c
PrivacyIsARight <vexalous@gmail.com>
2026-05-17 00:49:19 -0400

    Refactor byte swap definitions for GCC

--- src/base/tools/bswap_64.h
+++ src/base/tools/bswap_64.h
@@ -19,13 +19,7 @@
 #ifndef RXS_BSWAP_64_H
 #define RXS_BSWAP_64_H
 
-#ifdef _MSC_VER
-
-#include <stdlib.h>
-#define bswap_64(x) _byteswap_uint64(x)
-#define bswap_32(x) _byteswap_ulong(x)
-
-#elif defined __GNUC__
+#ifdef __GNUC__
 
 #define bswap_64(x) __builtin_bswap64(x)
 #define bswap_32(x) __builtin_bswap32(x)
