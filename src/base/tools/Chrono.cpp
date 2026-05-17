c0b1b149914c6ae0057ba11e961d97c8483c44f2
PrivacyIsARight <vexalous@gmail.com>
2026-05-17 00:48:28 -0400

    Refactor Chrono to remove Windows-specific code
    Removed Windows-specific high-resolution timer code.

--- src/base/tools/Chrono.cpp
+++ src/base/tools/Chrono.cpp
@@ -19,25 +19,13 @@
 #include "Chrono.h"
 
 
-#ifdef RXS_OS_WIN
-#   include <Windows.h>
-#endif
-
-
 namespace rxs {
 
 
 double Chrono::highResolutionMSecs()
 {
-#   ifdef RXS_OS_WIN
-    LARGE_INTEGER f, t;
-    QueryPerformanceFrequency(&f);
-    QueryPerformanceCounter(&t);
-    return static_cast<double>(t.QuadPart) * 1e3 / f.QuadPart;
-#   else
     using namespace std::chrono;
     return static_cast<uint64_t>(duration_cast<nanoseconds>(high_resolution_clock::now().time_since_epoch()).count()) / 1e6;
-#   endif
 }
 
 
