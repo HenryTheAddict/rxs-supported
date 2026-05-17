c3878485c76352cb6bf96ed16a41dfaaae26b063
PrivacyIsARight <vexalous@gmail.com>
2026-05-17 00:01:58 -0400

    Clean up version.h by removing obsolete OS and MSVC checks
    Removed MSVC version definitions and OS checks for Windows, iOS, and macOS.

--- src/version.h
+++ src/version.h
@@ -21,35 +21,7 @@
 #define APP_VER_MINOR 0
 #define APP_VER_PATCH 1
 
-#ifdef _MSC_VER
-#   if (_MSC_VER >= 1950)
-#       define MSVC_VERSION 2026
-#   elif (_MSC_VER >=1930 && _MSC_VER < 1950)
-#       define MSVC_VERSION 2022
-#   elif (_MSC_VER >= 1920 && _MSC_VER < 1930)
-#       define MSVC_VERSION 2019
-#   elif (_MSC_VER >= 1910 && _MSC_VER < 1920)
-#       define MSVC_VERSION 2017
-#   elif _MSC_VER == 1900
-#       define MSVC_VERSION 2015
-#   elif _MSC_VER == 1800
-#       define MSVC_VERSION 2013
-#   elif _MSC_VER == 1700
-#       define MSVC_VERSION 2012
-#   elif _MSC_VER == 1600
-#       define MSVC_VERSION 2010
-#   else
-#       define MSVC_VERSION 0
-#   endif
-#endif
-
-#ifdef RXS_OS_WIN
-#    define APP_OS "Windows"
-#elif defined RXS_OS_IOS
-#    define APP_OS "iOS"
-#elif defined RXS_OS_MACOS
-#    define APP_OS "macOS"
-#elif defined RXS_OS_ANDROID
+#ifdef RXS_OS_ANDROID
 #    define APP_OS "Android"
 #elif defined RXS_OS_LINUX
 #    define APP_OS "Linux"
