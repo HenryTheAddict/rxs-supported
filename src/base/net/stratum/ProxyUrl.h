7bcb8f8297d3db0ac7f5965fbbd340583fe883c1
PrivacyIsARight <vexalous@gmail.com>
2026-05-22 19:08:43 -0400

    Add [[nodiscard]] to isValid and host methods

--- src/base/net/stratum/ProxyUrl.h
+++ src/base/net/stratum/ProxyUrl.h
@@ -33,9 +33,9 @@ public:
 
     ProxyUrl(const rapidjson::Value &value);
 
-    inline bool isValid() const { return m_port > 0 && (m_scheme == UNSPECIFIED || m_scheme == SOCKS5); }
+    [[nodiscard]] inline bool isValid() const { return m_port > 0 && (m_scheme == UNSPECIFIED || m_scheme == SOCKS5); }
 
-    const String &host() const;
+    [[nodiscard]] const String &host() const;
     rapidjson::Value toJSON(rapidjson::Document &doc) const;
 };
 
