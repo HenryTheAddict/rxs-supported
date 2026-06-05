58bfb7d58a3760eeb42b8e2d6140131465318bcc
h3nry <1324902498a@gmail.com>
2026-06-05 12:43:50 -0400

    Improve CMake definition handling

--- cmake/asm.cmake
+++ cmake/asm.cmake
@@ -3,11 +3,11 @@ set(RXS_ASM_SOURCES "")
 set(RXS_ASM_LIBRARY "")
 
 if (WITH_ASM AND NOT RXS_ARM AND NOT RXS_RISCV AND CMAKE_SIZEOF_VOID_P EQUAL 8)
-    add_definitions(/DRXS_FEATURE_ASM)
+    add_definitions(-DRXS_FEATURE_ASM)
     set(RXS_ASM_SOURCES
         src/crypto/common/Assembly.h
         src/crypto/common/Assembly.cpp
     )
 else()
-    remove_definitions(/DRXS_FEATURE_ASM)
+    remove_definitions(-DRXS_FEATURE_ASM)
 endif()
